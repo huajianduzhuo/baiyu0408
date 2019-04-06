94448f20307d4d2ee1021fb8a0d4b0f069ed7888
huajianduzhuo <yujing_m4370@163.com>
2019-04-06 14:12:20 +0800

    feat: 解决配置history mode的问题，添加自动重启服务的命令

--- app.js
+++ app.js
@@ -6,25 +6,23 @@ const router = require('./routes')
 const app = new Koa()
 
 // static files
-const main = require('koa-static')(path.join(__dirname, '../public'))
+const main = require('koa-static')(path.join(__dirname, '/baiyu0408fe/dist/'))
 app.use(main)
 
-// body-parser
-app.use(bodyParser())
-
+// adapt vue-router history mode
+const historyRoutesReg = /\.[a-zA-Z]+$/
 app.use(async (ctx, next) => {
   await next()
-  if (ctx.status === 404) {
+  if (ctx.status === 404 && !historyRoutesReg.test(ctx.path)) {
     ctx.status = 200
     ctx.type = 'html'
     ctx.body = fs.createReadStream('./baiyu0408fe/dist/index.html')
   }
 })
 
-/* app.use(async (ctx, next) => {
-  ctx.body = ctx.request.body
-  next()
-}) */
+// body-parser
+app.use(bodyParser())
+
 // router
 app
   .use(router.routes())
--- routes.js
+++ routes.js
@@ -1,7 +1,10 @@
-const router = require('koa-router')()
-
-router.get('getUsers', (ctx, next) => {
-  
+const Router = require('koa-router')
+const router = new Router({
+  prefix: '/api'
+})
+router.use(async (ctx, next) => {
+  ctx.body = ctx.request.body
+  next()
 })
 
 module.exports = router
